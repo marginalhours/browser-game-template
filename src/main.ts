c638ca62000f2d9d432cfe1b516757f003d16a3c
marginalhours <8655118+marginalhours@users.noreply.github.com>
2023-04-14 11:44:35 +0100

    3 files modified

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,58 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter'
+import './style.css';
+import kontra from 'kontra';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const { init, Sprite, GameLoop } = kontra;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// const canvas = document.getElementById('#game-canvas');
+const canvasElement: HTMLCanvasElement =
+  document.getElementById('#game-canvas');
+const { context, canvas } = init(canvasElement);
+
+let sprite = Sprite({
+  x: 100, // starting x,y position of the sprite
+  y: 80,
+  color: 'red', // fill color of the sprite rectangle
+  width: 20, // width and height of the sprite rectangle
+  height: 40,
+  dx: 2, // move the sprite 2px to the right every frame
+});
+
+let loop = GameLoop({
+  // create the main game loop
+  update: function () {
+    // update the game state
+    sprite.update();
+
+    // wrap the sprites position when it reaches
+    // the edge of the screen
+    if (sprite.x > canvas.width) {
+      sprite.x = -sprite.width;
+    }
+  },
+  render: function () {
+    // render the game state
+    sprite.render();
+  },
+});
+
+loop.start(); // start the game
+
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://www.typescriptlang.org/" target="_blank">
+//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+//     </a>
+//     <h1>Vite + TypeScript</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite and TypeScript logos to learn more
+//     </p>
+//   </div>
+// `
+
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
