555a8963194e96470c8baaf9835a52d671df9066
marginalhours <8655118+marginalhours@users.noreply.github.com>
2023-04-26 16:52:25 +0100

    1 file modified

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/browser-game-template/',
+  build: {
+    assetsInlineLimit: 0, // don't inline stuff! breaks the build
+  },
 });
