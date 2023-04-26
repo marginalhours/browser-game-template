4911e2ac86ab4b37dc1422e7ffdc74f9570cf1b6
marginalhours <8655118+marginalhours@users.noreply.github.com>
2023-04-26 10:43:03 +0100

    3 files modified

--- src/sounds.ts
+++ src/sounds.ts
@@ -1,1 +1,15 @@
 import { Howl } from 'howler';
+
+import bounceSrc from './assets/sounds/bounce';
+
+const bounceSound = new Howl({
+  src: [bounceSrc],
+  autoplay: false,
+  loop: false,
+  rate: 1.0,
+  volume: 0.25,
+});
+
+export const playBounce = () => {
+  bounceSound.play();
+};
